c895c92177ff8e2f7e993e1c545cfe356fe43a9a
Jack Gilcrest <me@jp4g.llc>
2025-02-03 18:41:30 -0700

    poc

---
+++ packages/noir/regex_gen.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+CURRENT_DIR="$(pwd)"
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd $SCRIPT_DIR
+
+gen_regex() {
+    # Name of circuit to set
+    circuit_name=$(echo "${1%.json}" | sed -r 's/(^|_)(.)/\U\2/g')
+    # Name of file to set
+    file_name="${1%.json}_regex.nr"
+
+    # Gen regex
+    zk-regex decomposed \
+        -d "$1" \
+        --noir-file-path "../$file_name" \
+        -t "$circuit_name" \
+        -g true \
+        --sparse-array false
+}
+
+cd src/templates/
+
+for file in *.json; do
+    gen_regex "$file"
+done
+
+
+cd $CURRENT_DIR
--- x/info.sh
+++ x/info.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "============[Sparse Array Regex]============"
-cd sparse
-nargo compile --force --silence-warnings
-nargo info
-bb gates -b target/sparse_regex.json | grep "circuit"
-cd ..
-
-# echo "============[Simple Array Regex]============"
-# cd simple
+# echo "============[Sparse Array Regex]============"
+# cd sparse
 # nargo compile --force --silence-warnings
 # nargo info
-# bb gates -b target/simple_regex.json | grep "circuit"
+# bb gates -b target/sparse_regex.json | grep "circuit"
 # cd ..
+
+echo "============[Simple Array Regex]============"
+cd simple
+nargo compile --force --silence-warnings
+nargo info
+bb gates -b target/simple_regex.json | grep "circuit"
+cd ..
